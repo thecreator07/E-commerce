a76070531a5c6a5a13ee3729b6f9fdaf90740839
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2024-06-30 02:37:42 +0530

    application deployed

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ['http://localhost:5173', 'https://e-commerce-woad-rho.vercel.app'] || "*", //urls to give access
+        origin: ['https://e-commerce-bkd2.onrender.com','http://localhost:5173'], //urls to give access
         credentials: true,
     })
 );
