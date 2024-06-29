83f1c536b493a0d025aa474477dc83e37877dda6
Aman Kumar Prasad <amankumarprasad72@gmail.com>
2024-06-30 02:22:21 +0530

    deployed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,19 +1,28 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
+import { fileURLToPath } from "url"
 
 const app = express();
 
 app.use(
     cors({
-        origin: ['http://localhost:5173'], //urls to give access
+        origin: ['http://localhost:5173'] || "*", //urls to give access
         credentials: true,
     })
 );
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+console.log(__dirname)
+// console.log(dummy)
 app.use(express.json({ limit: "20kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "../dist")))
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../dist/index.html"))
+})
 
 app.use(cookieParser());
 //Router import
--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-// const API_URI = import.meta.env.PROD ? window.location.origin : `http://localhost:8000`
-const API_URI = `http://localhost:8000`
+const API_URI = import.meta.env.PROD ? window.location.origin : `http://localhost:8000`
+// const API_URI = `http://localhost:8000`
 
 export {API_URI}
